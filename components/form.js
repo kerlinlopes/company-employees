bd7729c6dd3ea1010f8aba3b7f02b8ab0aa40276
Kerlin <lopeskerlin@gmail.com>
2021-12-22 20:53:04 -0500

    done

--- components/form.js
+++ components/form.js
@@ -77,19 +77,133 @@ export default function Form() {
 				</Card>
 			</div>
 
-            <div className={styles.card}>
+			<div className={styles.card}>
 				<Card style={{ width: "18rem" }}>
-					<Card.Body>
-						<Card.Title>Kerlin</Card.Title>
-						<Card.Subtitle className="mb-2 text-muted">
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Vicente Fernandez</Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
 							WORK PROGRESS
 						</Card.Subtitle>
-						<Card.Text>He sucks so bad but its ok I know he's trying</Card.Text>
-						<Card.Link href="#">Card Link</Card.Link>
-						<Card.Link href="#">Another Link</Card.Link>
+						<Card.Text className={styles.progress}>He is all legend that is all</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 02-17-1940</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Aquarius</Card.Text>
 					</Card.Body>
 				</Card>
 			</div>
+
+			<div className={styles.card}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Abel Tesfaye</Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
+							WORK PROGRESS
+						</Card.Subtitle>
+						<Card.Text className={styles.progress}>Very talented and hardworking needs a raise</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 02-16-1990</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Aquarius</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+
+			<div className={styles.card}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Jamilet Nino</Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
+							WORK PROGRESS
+						</Card.Subtitle>
+						<Card.Text className={styles.progress}>The baddest bish ever she deserves a million dollars</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 10-08-2000</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Libra</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+
+			<div className={styles.card}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Courtlynn Booker</Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
+							WORK PROGRESS
+						</Card.Subtitle>
+						<Card.Text className={styles.progress}>Super smart she needs to be a manager</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 12-23-2000</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Capricorn</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+
+			<div className={styles.card}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Kylie Jenner</Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
+							WORK PROGRESS
+						</Card.Subtitle>
+						<Card.Text className={styles.progress}>shes such a leo i dont even know why she works here shes already super rich</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 08-10-1997</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Leo</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+
+			<div className={styles.card}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Friday </Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
+							WORK PROGRESS
+						</Card.Subtitle>
+						<Card.Text className={styles.progress}>he is the best dog ever he deserves to be the boss</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 04-20-2018</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Taurus</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+
+			<div className={styles.card}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Luna </Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
+							WORK PROGRESS
+						</Card.Subtitle>
+						<Card.Text className={styles.progress}>the most smallest but cutest dog ever</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 02-20-2020</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Pisces</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+
+			<div className={styles.card}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Jaydi Lopez </Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
+							WORK PROGRESS
+						</Card.Subtitle>
+						<Card.Text className={styles.progress}>the worst employee ever i hate her and im firing her tomorrow</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 11-21-2004</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Scorpio</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+
+			<div className={styles.card}>
+				<Card style={{ width: "18rem" }}>
+					<Card.Body className={styles.body}>
+						<Card.Title className={styles.name}>Breily Marquez </Card.Title>
+						<Card.Subtitle className={styles.subtitle}>
+							WORK PROGRESS
+						</Card.Subtitle>
+						<Card.Text className={styles.progress}>super smart, already promoted 45 times this year, love her</Card.Text>
+                        <Card.Text className={styles.birthday}>DOB: 06-13-2003</Card.Text>
+                        <Card.Text className={styles.horoscope}>Sign: Gemini</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+
+
 		</div>
 	);
 }
--- pages/topinfo.js
+++ pages/topinfo.js
@@ -1,27 +1,30 @@
 import React from "react";
-import styles from '../styles/TopInfo.module.css'
-import Form from "../components/form"
+import styles from "../styles/TopInfo.module.css";
+import Form from "../components/form";
 import Link from "next/dist/client/link";
 
-export default function TopInfo(){
-    return(
-        <div className={styles.container}>
-            <Form/>
-            <div  className={styles.header} >
-				<Link href="/">
-					HOME
-				</Link>{" "}
+export default function TopInfo() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<Link href="/">HOME</Link>{" "}
 			</div>
-            <div  className={styles.header} >
-				<Link href="/employeelist">
-					EMPLOYEES
-				</Link>{" "}
+			<div className={styles.header}>
+				<Link href="/employeelist">EMPLOYEES</Link>{" "}
 			</div>
-            <div  className={styles.header} >
-				<Link href="/about">
-					ABOUT
-				</Link>{" "}
+			<div className={styles.header}>
+				<Link href="/about">ABOUT</Link>{" "}
 			</div>
-        </div>
-    )
+			<Form />
+			<div className={styles.header}>
+				<Link href="/">HOME</Link>{" "}
+			</div>
+			<div className={styles.header}>
+				<Link href="/employeelist">EMPLOYEES</Link>{" "}
+			</div>
+			<div className={styles.header}>
+				<Link href="/about">ABOUT</Link>{" "}
+			</div>
+		</div>
+	);
 }
